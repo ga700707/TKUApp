6f5fa59254eefbd336db84dd98536014cdae0471
ga700707 <ga700707@gmail.com>
2019-12-14 21:20:54 +0800

    a

--- lib/PageView/Page1Style.dart
+++ lib/PageView/Page1Style.dart
@@ -7,3 +7,20 @@ TextStyle btnStyle() {
       fontFamily: "Fredericka the Great",);
   return result;
 }
+
+TextStyle tokenStyle() {
+  var result = TextStyle(
+      color: Colors.black,
+      fontSize: 20.0,
+      fontFamily: "Fredericka the Great",);
+  return result;
+}
+
+
+TextStyle tokenHintStyle() {
+  var result = TextStyle(
+      color: Colors.grey,
+      fontSize: 20.0,
+      fontFamily: "Fredericka the Great",);
+  return result;
+}
--- lib/PageView/Page1Widget.dart
+++ lib/PageView/Page1Widget.dart
@@ -6,11 +6,12 @@ import 'package:ctestapp/PageView/Page1Style.dart';
 import 'package:ctestapp/PageView/PageViewCT.dart';
 
 import 'package:flutter/material.dart';
+final TextEditingController TokenCT = new TextEditingController();
 
 Widget createLoginBtn(context) {
   var result = SizedBox(
-    height: 3*MaxSize.height / 4,
-    width: 3*MaxSize.width / 4,
+    height: 3 * MaxSize.height / 4,
+    width: 3 * MaxSize.width / 4,
     child: RaisedButton(
       color: Colors.red[400],
       splashColor: Colors.white,
@@ -20,14 +21,10 @@ Widget createLoginBtn(context) {
         style: btnStyle(),
       ),
       shape: CircleBorder(
-        side: BorderSide(
-          color: Colors.white,
-          width: MaxSize.width/70),
-        
+        side: BorderSide(color: Colors.white, width: MaxSize.width / 70),
       ),
-      
       onPressed: () async {
-        //login(context);
+        login(context);
       },
     ),
   );
@@ -35,17 +32,50 @@ Widget createLoginBtn(context) {
   return result;
 }
 
-Future<void> login(context) async {
-  Map<String, String> jsonString = {
-    "Type": "",
-  };
-  print(jsonString);
-  var result = await HttpService.postTest(
-      Constant.Chat_Api_URL + "member/login", jsonString);
-  if (result == null) return;
-  var jsonStr = json.decode(result);
-  print(jsonStr);
-  print(jsonStr['id']);
-  Navigator.push(
-      context, MaterialPageRoute(builder: (context) => PageViewCT()));
+login(context) {
+  showDialog(
+      context: context,
+      builder: (context) {
+        return new AlertDialog(
+          title: new Text("進入考場"),
+          content: Padding(
+            padding: EdgeInsets.symmetric(vertical: MaxSize.height / 80),
+            child: tokenFiled(),
+          ),
+          actions: <Widget>[
+            new FlatButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: new Text("取消"),
+            ),
+            new FlatButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: new Text("確認"),
+            ),
+          ],
+        );
+      });
+}
+
+Widget tokenFiled() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: MaxSize.width / 50),
+    alignment: Alignment.center,
+    height: MaxSize.height / 10,
+    decoration: new BoxDecoration(
+        color: Colors.white70,
+        border: new Border.all(color: Colors.blueGrey, width: 4.0),
+        borderRadius: new BorderRadius.circular(12.0)),
+    child: new TextFormField(
+      controller: TokenCT,
+      decoration: InputDecoration.collapsed(
+        hintText: '認證碼',
+        hintStyle: tokenHintStyle(),
+      ),
+      style: tokenStyle(),
+    ),
+  );
 }
